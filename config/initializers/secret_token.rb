b963f03272ba739b0b4837ce06491c18090c8b2e
Adam Jonas <francestown81@gmail.com>
2012-04-29 19:30:24 -0300

    Update config/initializers/secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,3 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Collegeapp::Application.config.secret_token = '3922fba77146a74ae945f59ac58a872caeb5c4596645b62779a8870b9860d2d981dd4b4f7b4cd296e6e244446efbf3d2a7cd3138e578c8b32a3f07ccb5a19c0f'
